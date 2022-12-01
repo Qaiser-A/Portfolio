50456f7fcc40f7190313f37f0d49dae02dfb95a2
Qaiser-A <58219334+Qaiser-A@users.noreply.github.com>
2022-12-01 00:31:52 +0000

    Create Conditionals.py
    Very basic program using conditionals without user input

---
+++ Python/Conditionals.py
@@ -0,0 +1,15 @@
+# Conditionals
+
+#Setting the age to 25 and storing it in a variable called "age"
+age = 25
+
+#If the age is less than 25
+if age < 26:
+    #Output the following to the console
+    print('You are younger than 26')
+#Else if age is less than 24
+elif age > 24:
+    #Output the following
+    print ('You are older than 24')
+else:
+    print('You clearly have a different age')
